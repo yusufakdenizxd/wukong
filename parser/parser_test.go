6b96f27fd8f6002cef7d5b466c443dc15728e5fb
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-09-28 22:12:29 +0300

    test(parser): test for let statement parser added

---
+++ parser/parser_test.go
@@ -0,0 +1,65 @@
+package parser
+
+import (
+	"testing"
+	"wukong.com/ast"
+	"wukong.com/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x=5;
+	let y=10;
+	let foobar=831234;
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatements(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatements(t *testing.T, s ast.Statement, expectedIdentifier string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != expectedIdentifier {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", expectedIdentifier, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != expectedIdentifier {
+		t.Errorf("s.Name not '%s'. got=%s", expectedIdentifier, letStmt.Name)
+		return false
+	}
+
+	return true
+}
