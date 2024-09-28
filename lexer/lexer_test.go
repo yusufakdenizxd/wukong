fe31789cca260109b55373c0bb62c1e35880de88
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-09-28 22:12:28 +0300

    test(eq and noteq): lexer test for eq and not eq added

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,6 +21,9 @@ if(5 < 10){
 }else{
 	return false;
 }
+
+10 == 10;
+10 != 9;
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -91,6 +94,15 @@ if(5 < 10){
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
 	}
 
