d30da24609a45676e3980d574d31d7762c47d1d3
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-09-28 22:43:23 +0300

    feat(integer literal): integer literal added to ast to use for parser

--- ast/ast.go
+++ ast/ast.go
@@ -124,3 +124,18 @@ func (es *ExpresssionStatement) String() string {
 	}
 	return ""
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
