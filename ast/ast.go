3ccbf982af8e10a88f13ee5e49d833a837f23a74
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-09-28 22:12:29 +0300

    feat(ast module): abstract syntax tree module added

---
+++ ast/ast.go
@@ -0,0 +1,54 @@
+package ast
+
+import (
+	"wukong.com/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expresssionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) expressionNode() {}
+
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
