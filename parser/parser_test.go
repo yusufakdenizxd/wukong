09331d8703ef6051e232ce44ed76c9d614cc9e68
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-10-02 22:17:49 +0300

    test(boolean tests): boolean tests added to prefix and infix expression tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -170,12 +170,14 @@ func TestIntegerLiteralExpression(t *testing.T) {
 
 func TestParsingPrefixExpressions(t *testing.T) {
 	prefixTests := []struct {
-		input        string
-		operator     string
-		integerValue int64
+		input    string
+		operator string
+		value    interface{}
 	}{
 		{"!5;", "!", 5},
 		{"-15;", "-", 15},
+		{"!true;", "!", true},
+		{"!false;", "!", false},
 	}
 
 	for _, tt := range prefixTests {
@@ -204,9 +206,7 @@ func TestParsingPrefixExpressions(t *testing.T) {
 			t.Fatalf("exp.Operator is not '%s'. got :%s", tt.operator, exp.Operator)
 		}
 
-		if !testIntegerLiteral(t, exp.Right, tt.integerValue) {
-			return
-		}
+		testLiteralExpression(t, exp.Right, tt.value)
 	}
 }
 
@@ -255,9 +255,9 @@ func testBoolLiteral(t *testing.T, il ast.Expression, value bool) bool {
 func TestParsingInfixExpressions(t *testing.T) {
 	infixTests := []struct {
 		input      string
-		leftValue  int64
+		leftValue  interface{}
 		operator   string
-		rightValue int64
+		rightValue interface{}
 	}{
 		{"5 + 5;", 5, "+", 5},
 		{"5 - 5;", 5, "-", 5},
@@ -267,6 +267,9 @@ func TestParsingInfixExpressions(t *testing.T) {
 		{"5 < 5;", 5, "<", 5},
 		{"5 == 5;", 5, "==", 5},
 		{"5 != 5;", 5, "!=", 5},
+		{"true == true", true, "==", true},
+		{"false != true", false, "!=", true},
+		{"false == false", false, "==", false},
 	}
 
 	for _, tt := range infixTests {
