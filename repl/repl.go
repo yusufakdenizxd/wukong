c4512d1f16479145f005d76f991a236d732581d4
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-07 17:50:49 +0300

    feat(repl): parseing added to repl instead of lexing

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+
 	"wukong.com/lexer"
-	"wukong.com/token"
+	"wukong.com/parser"
 )
 
 const PROMPT = "-> "
@@ -22,10 +23,25 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 
 }
