b93168c8ff7d39ffe137a2b7be3c5f1d3155d586
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-09 00:57:41 +0300

    refactor(repl): repl now printing evaluation results instead of program

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"wukong.com/evaluator"
 	"wukong.com/lexer"
 	"wukong.com/parser"
 )
@@ -32,9 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
-
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 
 }
