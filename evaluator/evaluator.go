88fd56aae3bc0a9ca546c4cc3b58379aabbbd6e4
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-10 17:42:11 +0300

    feat(if eval): eval of if expression added

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -33,6 +33,12 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.BooleanLiteral:
 		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
@@ -127,6 +133,17 @@ func evalIntegerInfixExpressiond(operator string, left object.Object, right obje
 		return NULL
 	}
 }
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	}
+	return NULL
+}
+
 func isTruthy(obj object.Object) bool {
 	switch obj {
 	case NULL:
