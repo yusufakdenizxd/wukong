f62282832604bd3bdd1bfda69f469ca657ac60cc
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-11 19:50:42 +0300

    feat(enviroment): enviroment object added for variable setting and getting

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"wukong.com/evaluator"
 	"wukong.com/lexer"
+	"wukong.com/object"
 	"wukong.com/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = "-> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -33,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
