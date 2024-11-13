6f76f14f17fd18c02fb06cb8a1dd27028379ba45
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-13 21:16:57 +0300

    test(string token): string token lexer test added

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -24,6 +24,9 @@ if(5 < 10){
 
 10 == 10;
 10 != 9;
+
+"foobar"
+"foo bar"
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -102,6 +105,8 @@ if(5 < 10){
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 
 		{token.EOF, ""},
 	}
