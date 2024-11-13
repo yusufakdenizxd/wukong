bc1c03131060853f4d9a54768beb6b37c7dcd043
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-13 21:47:17 +0300

    style(string eval): tests for string evaluation added

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -193,6 +193,7 @@ func TestErrorHandling(t *testing.T) {
 
 			`, "unknown operator: BOOLEAN + BOOLEAN"},
 		{"foobar", "identifier not found: foobar"},
+		{`"foo" - "bar"`, "unknown operator: STRING - STRING"},
 	}
 
 	for _, tt := range tests {
@@ -292,6 +293,18 @@ func TestStringLiteral(t *testing.T) {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
 
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!";`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
