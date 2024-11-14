eedec7d5fad5fa91faac1832fa2c065d7109196d
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-14 16:41:06 +0300

    feat(array index eval): index operator of arrays added to evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -118,6 +118,18 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 		return &object.Array{Elements: elements}
 
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+
+		return evalIndexExpression(left, index)
 	}
 
 	return nil
@@ -329,6 +341,28 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 		result = append(result, evaluated)
 	}
 	return result
+}
+
+func evalIndexExpression(left object.Object, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array object.Object, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+
+	idx := index.(*object.Integer).Value
+	maxIndex := int64(len(arrayObject.Elements) - 1)
+	if idx < 0 || idx > maxIndex {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
+
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
