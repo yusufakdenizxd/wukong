69c46d86fa677c1a1cee8e603c9f6aa12b9c69e1
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-14 12:49:45 +0300

    test(array lexing): test expanding for array literal

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -27,6 +27,7 @@ if(5 < 10){
 
 "foobar"
 "foo bar"
+[1,2];
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -107,6 +108,12 @@ if(5 < 10){
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRAKET, "]"},
+		{token.SEMICOLON, ";"},
 
 		{token.EOF, ""},
 	}
