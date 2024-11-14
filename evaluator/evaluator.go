797b7e70e7a7432b31bfdbf449311e309398ffb8
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-14 21:22:51 +0300

    feat(hash eval): evalaution of hash

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -190,6 +190,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return evalIndexExpression(left, index)
+
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	}
 
 	return nil
@@ -410,6 +413,32 @@ func evalIndexExpression(left object.Object, index object.Object) object.Object 
 	default:
 		return newError("index operator not supported: %s", left.Type())
 	}
+}
+
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyN, valueN := range node.Pairs {
+		key := Eval(keyN, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueN, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
 
 func evalArrayIndexExpression(array object.Object, index object.Object) object.Object {
