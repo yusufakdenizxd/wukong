0f4c7fe9d0fa5176f9afdec72f05838f1c23a57f
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-11-14 12:49:22 +0300

    feat(array lexing): lexing of array with LBRACKET and RBRACKET added

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -88,6 +88,10 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRAKET, l.ch)
 	case '=':
 		if l.peekChar() == '=' {
 			ch := l.ch
